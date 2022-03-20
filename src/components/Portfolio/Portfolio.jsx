b4b5cb0e87530fa3cb623ea7f7eb88c036250f65
prokash Pul <pprokash102@gmail.com>
2022-03-20 15:00:51 +0600

    update

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Portfolio.css";
 import JsonData from "../data/portfilio.json";
 import Singleportfolio from "../Singleportfolio/Singleportfolio";
